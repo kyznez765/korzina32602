e82149453bdb6532b97b58ef9713afccf03efcc1
kyznez_76 <kyznez76A@yandex.ru>
2024-01-27 15:34:58 +0300

    добавили телеграм(3)

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
+import telebot
+import requests
+from .forms import *
+
+
 # Create your views here.
 
 
@@ -10,13 +15,15 @@ def index(req):
     return render(req, 'index.html', data)
 
 
+
+
 def toCart(req):
     items = Cart.objects.filter(user=req.user)
     forma = OrderForm()
     total = 0
     for i in items:
         total += i.calcSumma()
-    total = round(total,2)  # округление цены
+    total = round(total, 2)  # округление цены
     if req.POST:
         forma = OrderForm(req.POST)
         k1 = req.POST.get('adres')
@@ -28,20 +35,26 @@ def toCart(req):
             k2 = forma.cleaned_data.get('tel')
             k3 = forma.cleaned_data.get('email')
             print(k1, k2, k3)
-            myzakaz=''
+            myzakaz = ''
             for one in items:
-                myzakaz += one.tovar.name+' '
-                myzakaz += 'количество '+str(one.count)+' '
-                myzakaz += 'сумма '+str(one.summa)+' '
-                myzakaz += 'скидка '+str(one.tovar.discount)+' '
-            Order.objects.create(adres=k1, tel=k2, email=k3,
-                                 total=total, myzakaz='myazakaz',
+                myzakaz += one.tovar.name + ' '
+                myzakaz += 'количество ' + str(one.count) + ' '
+                myzakaz += 'сумма ' + str(one.summa) + ' '
+                myzakaz += 'скидка ' + str(one.tovar.discount) + ' '
+            neworder = Order.objects.create(adres=k1, tel=k2, email=k3,
+                                 total=total, myzakaz = myzakaz,
                                  user=req.user)
             items.delete()
-            return render(req,'sps.html')
+            ##################################################################################
+            #в телеграм
+            telegram(neworder)
+
+
+            return render(req, 'sps.html')
+
+
     data = {'tovari': items, 'total': total, 'formaorder': forma}
     return render(req, 'cart.html', data)
-
 def buy(req, id):
     item = Tovar.objects.get(id=id)
     curuser = req.user
@@ -52,7 +65,7 @@ def buy(req, id):
         getTovar.save()
     else:
         Cart.objects.create(tovar=item, count=1, user=curuser, summa=item.price)
-    data = {}
+        data = {}
     return redirect('home')
 
 
@@ -71,3 +84,27 @@ def cartCount(req, num, id):
     item.summa = item.calcSumma()
     item.save()
     return redirect('tocart')
+
+
+# def telegram(neworder):
+#     token = '6912404517:AAGymoL3sJpf10K3bOUF3xokTPM47lrDOgI'
+#     #t.me/
+#     chat = '528849379'
+#     message = neworder.user.username + ' '+neworder.tel+ ' '+neworder.myzakaz
+#     bot = telebot.TeleBot(chat,'новый заказ')
+#     bot.send_message(chat, message)
+
+    # 6912404517:AAGymoL3sJpf10K3bOUF3xokTPM47lrDOgI   токен телеграмм бота
+    # t.me/pogodka2023_bot   канал телеграмм бота
+
+    #новый  6913165680:AAEZERrWPJNyGoE8t3bzgULvO1iu8AMCsro
+    #t.me/korzina326_bot
+
+import requests
+def telegram(neworder):
+        token = '6913165680:AAEZERrWPJNyGoE8t3bzgULvO1iu8AMCsro'
+        # t.me/turtle3000_bot
+        chat = '528849379'
+        message = neworder.user.username + ' ' + neworder.tel + ' ' + neworder.myzakaz
+        url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat}&text={message}"
+        requests.get(url)
